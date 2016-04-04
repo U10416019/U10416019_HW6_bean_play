331d18501858df41ae9f90e64855d0e8ed115d8b
WenChi <u10416019@go.utaipei.edu.tw>
2016-04-04 18:06:37 +0800

    Create a main class
    This file is the main class of the bean game, it just call the ActionBeanGame class then run it.

---
+++ TestActionBeanGame.java
@@ -0,0 +1,10 @@
+//U10416019鄭文琪
+
+import javafx.application.Application;
+
+public class TestActionBeanGame{
+	public static void main(String[] args){
+		//call the BeanGame class
+		Application.launch(ActionBeanGame.class, args);
+	}
+}
